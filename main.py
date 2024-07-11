de81da34ca8ffb53f657fff22f94d3dfd0aef25c
Ryan Adrian <NayrAdrian24@gmail.com>
2024-07-11 15:39:06 +0800

    V2. Added a Search Function, Checks the stored data(JSON) and shows the email and password.

--- main.py
+++ main.py
@@ -61,6 +61,24 @@ def save():
             password_entry.delete(0, END)
 
 
+# ---------------------------- SEARCH WEBSITE PASSWORD ------------------------------- #
+
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(title=website, message=f"Email: {email}\nPassword: {password}")
+        else:
+            messagebox.showinfo(title="Error", message=f"No details for {website} exists.")
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -81,8 +99,8 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=50)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=32)
+website_entry.grid(row=1, column=1, columnspan=1)
 website_entry.focus()  # Cursor on field when launched
 email_entry = Entry(width=50)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -93,6 +111,8 @@ password_entry.grid(row=3, column=1)
 # Buttons
 generate_password_button = Button(text="Generate Password", command=generate_password)
 generate_password_button.grid(row=3, column=2)
+search_button = Button(text="Search", command=find_password, width=14)
+search_button.grid(row=1, column=2)
 add_button = Button(text="Add", width=43, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
